ce84ab6e04cf6c9a0a10e71cc70e5716410b064f
yemeli237 <yemelizidane10@gmail.com>
2025-07-20 21:59:06 +0100

    interface de configuration et gestion de memoir
    l'interface principal a ete mise sur pier sans oublier une interface pour pour les parametre du logiciel

--- main/main.py
+++ main/main.py
@@ -1,6 +1,37 @@
 import customtkinter
+import os
+from PIL import Image, ImageTk
+from view.config import *
 
 #creation du fichier de configuration projet
+doc = os.path.join(os.path.expanduser("~"), "Documents")
+dossier = "Regcadas"
+save_dir = os.path.join(doc, dossier)
+if os.path.exists(save_dir) :
+    print("Creation du dossier de sauvegarde dans la bibliotheque Document")
+else:
+    os.mkdir(save_dir)
+    print("le dossier de sauvegarde existe deja")
+    
+config = {}
+#creation du fichier de configuration projet
+docs = os.path.join(os.path.expanduser("~"), "Documents/BT")
+dossiers = "Config"
+config_dir = os.path.join(docs, dossiers)
+# if os.path.exists(config_dir):
+#     pass
+# else:
+#     os.makedirs(config_dir)
+
+#charger le fichier des configuration
+#si le ficchier existe, on le charge et on applique les modifications
+if os.path.exists(config_dir):
+    if os.path.exists(f"{config_dir}/config.json"):
+        with open(f"{config_dir}/config.json", "r") as confg:
+            config = json.load(confg)
+        if config["theme"] != "":
+            customtkinter.set_appearance_mode(f"{config["theme"]}")
+            customtkinter.set_widget_scaling(config["scale"])
 
 
 #creation du dossier de sauvegarde projet
@@ -24,12 +55,169 @@ class App(customtkinter.CTk):
         #def la taille de l'afficharge
         self.geometry(f"{window_width}x{window_height}+{x}+{y}")
         #def une icone
-        self.iconbitmap("regcadas/res/icone.ico")
+        # self.iconbitmap("regcadas/res/icone.ico")
+        self.iconbitmap("res/icone.ico")
         
         # configuration des grige a afficharge 4x4
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure((2, 3), weight=0)
-        self.grid_rowconfigure((1, 1, 1), weight=0)
+        self.grid_rowconfigure((1, 1, 1), weight=1)
+        
+        # creer la barre laterale gauche grang fromat
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=400, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(4, weight=1)
+        
+        # creer la barre laterale gauche minimaliser
+        self.sidebar_frame_min = customtkinter.CTkFrame(self, width=100, corner_radius=0)
+        # self.sidebar_frame_min.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame_min.grid_rowconfigure(4, weight=1)
+        # self.sidebar_frame_min.forget()
+        
+        #chargement des icone avec PIL
+        param_img = Image.open("res/image/param.png")
+        data_img = Image.open("res/image/explore_data.png")
+        reduice_img = Image.open("res/image/reduice.png")
+        add_data_img = Image.open("res/image/add_data.png")
+        
+        
+        #configuration d l'image pour le btn parametre
+        self.img = customtkinter.CTkImage(light_image=param_img, dark_image=param_img, size=(30, 30))
+        #conficurationde l'image pour le btn des donne
+        self.explore_data_img = customtkinter.CTkImage(light_image=data_img, dark_image=data_img, size=(30, 30))
+        #conficurationde l'image pour le btn de reduction d'interface
+        self.reduice_frame_img = customtkinter.CTkImage(light_image=reduice_img, dark_image=reduice_img, size=(30, 30))
+        #conficurationde l'image pour le btn d'ajout des donnees
+        self.add_data_frame_img = customtkinter.CTkImage(light_image=add_data_img, dark_image=add_data_img, size=(30, 30))
+        
+        #bouton pour gerer les donnes
+        self.dashbor = customtkinter.CTkButton(
+            master=self.sidebar_frame,
+            text="Gestion", image=self.explore_data_img, 
+            width=30, 
+            height=30, 
+            corner_radius=3, 
+            bg_color="transparent", 
+            fg_color="transparent",
+            text_color="#DC3F3F",
+            command=self.agranddir
+        ).grid(row=1, column=0, padx=10, pady = 10)
+        
+        #bouton pour ajouter les nouvelle donne
+        self.add_data = customtkinter.CTkButton(
+            master=self.sidebar_frame,
+            text="Ajouter", image=self.add_data_frame_img, 
+            width=30, 
+            height=30, 
+            corner_radius=3, 
+            bg_color="transparent", 
+            fg_color="transparent",
+            text_color="#DC3F3F",
+            command=self.agranddir
+        ).grid(row=2, column=0, padx=10, pady = 10)
+        #btn pour ouvrir le menu
+        self.param_btn = customtkinter.CTkButton(
+            master=self.sidebar_frame,  
+            text="Parametre", image=self.img, 
+            width=30, 
+            height=30, 
+            corner_radius=3, 
+            bg_color="transparent", 
+            fg_color="transparent",
+            text_color="#DC3F3F",
+            command= self.config
+            )
+        self.param_btn.grid(row=8, column=0, padx=10, pady=10)
+        
+        #creer un bouton pour reduire la fenetre lateral gauche
+        self.close_btn = customtkinter.CTkButton(
+            master=self.sidebar_frame,
+            text="", image=self.reduice_frame_img, 
+            width=30, 
+            height=30, 
+            corner_radius=3, 
+            bg_color="transparent", 
+            fg_color="transparent",
+            command=self.reduice
+        ).grid(row=8, column=1)
+        
+        #afficharge lateralle reduite
+        #bouton pour gerer les donnes
+        self.dashbor = customtkinter.CTkButton(
+            master=self.sidebar_frame,
+            text="Gestion", image=self.explore_data_img, 
+            width=30, 
+            height=30, 
+            corner_radius=3, 
+            bg_color="transparent", 
+            fg_color="transparent",
+            text_color="#DC3F3F",
+            command=self.agranddir
+        ).grid(row=1, column=0, padx=10, pady = 10)
+        
+        ########################################bouton pour ajouter les nouvelle donne
+        #bouton pour gerer les donnees
+        self.add_data = customtkinter.CTkButton(
+            master=self.sidebar_frame_min,
+            text="", image=self.explore_data_img, 
+            width=30, 
+            height=30, 
+            corner_radius=3, 
+            bg_color="transparent", 
+            fg_color="transparent",
+            text_color="#DC3F3F",
+            command=self.agranddir
+        ).grid(row=1, column=0, padx=10, pady = 10)
+        ##bouton pour ajouter les nouvelle donne
+        self.add_data = customtkinter.CTkButton(
+            master=self.sidebar_frame_min,
+            text="", image=self.add_data_frame_img, 
+            width=30, 
+            height=30, 
+            corner_radius=3, 
+            bg_color="transparent", 
+            fg_color="transparent",
+            text_color="#DC3F3F",
+            command=self.agranddir
+        ).grid(row=2, column=0, padx=10, pady = 10)
+        #btn pour ouvrir le menu
+        self.param_btn = customtkinter.CTkButton(
+            master=self.sidebar_frame_min,  
+            text="", image=self.img, 
+            width=30, 
+            height=30, 
+            corner_radius=3, 
+            bg_color="transparent", 
+            fg_color="transparent",
+            text_color="#DC3F3F",
+            command= self.config
+            )
+        self.param_btn.grid(row=8, column=0, padx=10, pady=10)
+        
+
+        
+        
+        
+        
+    ############# section des fonctions
+    #fonction pour ouvrir l'interface des parametre
+    def config(self):
+        ouvrir_parametre(self)
+        
+    def agranddir(self):
+        largeur_actuel = self.sidebar_frame.cget("width")
+        nouvelle_largeur = largeur_actuel + 200
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame_min.grid_forget()
+    
+    #fonction pour reduire la fenaitre laterale gauche
+    def reduice(self):
+        print("reduite")
+        largeur_actuel = self.sidebar_frame.cget("width")
+        nouvelle_largeur = largeur_actuel - 100
+        self.sidebar_frame.grid_forget()
+        self.sidebar_frame_min.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        
 
 
 if __name__ == "__main__":
--- main/view/config.py
+++ main/view/config.py
@@ -1,1 +1,128 @@
+import customtkinter
+import json
+import os
+from tkinter import *
+from tkinter import filedialog
+import tkinter as TK
+from PIL import Image, ImageTk
+from tkinter import Tk, filedialog
 
+#creation du fichier de sauvegarde projet
+doc = os.path.join(os.path.expanduser("~"), "Documents/Regcadas")
+dossier = "Config"
+project_dir = os.path.join(doc, dossier)
+if os.path.exists(project_dir):
+    pass
+else:
+    os.makedirs(project_dir)
+placement = project_dir
+
+#verifier si le fichier de sauvegarde du theme est present
+if os.path.exists(f"{placement}/theme.json"):
+    pass
+else:
+    try:
+        with open(f"{placement}/theme.json", "w") as f:
+            json.dump({"theme":""}, f)
+    except Exception as e:
+        print(e)
+#verifier si le fichier de sauvegarde du l'echelle est present     
+if os.path.exists(f"{placement}/scale.json"):
+    pass
+else:
+    try:
+        with open(f"{placement}/scale.json", "w") as f:
+            json.dump({"scale":""}, f)
+    except Exception as e:
+        print(e)
+        
+def ouvrir_parametre(principal):#classe des parametres
+    self = customtkinter.CTkToplevel(principal)
+    self.title("Parametre")
+    self.geometry("1080x620+100+100")
+    self.attributes('-topmost', True)
+    self.resizable(False, False)
+    self.focus_force()
+    self.grid_columnconfigure(0, weight=1)
+    self.grid_columnconfigure((1, 1), weight=0)
+    self.grid_rowconfigure((1, 1, 1), weight=1)
+    
+    
+    logo_label = customtkinter.CTkLabel(#titre de la fenaitre
+        self, text="Parametre", 
+        font=customtkinter.CTkFont(size=30, weight="bold"),
+        text_color="#DC3F3F"
+        )
+    logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        
+    param_frame = customtkinter.CTkFrame(self, width=1000)#frame pour la configuration du theme
+    param_frame.grid(row=1, column=0 , sticky="ns")
+    #composant pour le parametre de theme
+    valeur_theme = ["System", "Dark", "Light"]
+        
+    theme_frame = customtkinter.CTkFrame(master=param_frame, width=500)
+    theme_frame.grid(row=1, column=0 , sticky="nsew", pady=10, padx=5,)
+    theme_lable = customtkinter.CTkLabel(master=theme_frame, text="Theme")
+    theme_lable.grid(row=0, column=3, sticky="nsew", padx=(100, 0), pady=(5, 10))
+    echele_theme = customtkinter.CTkSegmentedButton(
+            master=theme_frame, 
+            values=valeur_theme, 
+            command=lambda new_appearance_mode : apparence(new_appearance_mode),#fontion pour changer de them
+            width=1000
+            )
+    echele_theme.grid(row=1, column=3, sticky="nsew", padx=(100, 0))
+    
+    #parametre pour le composant echelle
+    valeur_echele = ["80%", "90%", "100%", "105%","110%","115%","120%","125%"]
+        
+    echele_frame = customtkinter.CTkFrame(master=param_frame, width=500)#frame pour changer d'echelle
+    echele_frame.grid(row=2, column=0 , sticky="nsew", pady=10, padx=5,)
+    echele_lable = customtkinter.CTkLabel(master=echele_frame, text="Echelle")
+    echele_lable.grid(row=0, column=0, sticky="nsew") 
+    echele_segment = customtkinter.CTkSegmentedButton(
+            master=echele_frame,
+            values=valeur_echele,
+            command=lambda new_scaling :echelle(new_scaling)
+            )
+    echele_segment.grid(row=1, column=0, sticky="nsew", padx=(0, 0), pady=(5, 10))
+    
+    
+            
+    save_btn = customtkinter.CTkButton(master=self, text="Sauvegarder", command=saves)
+    save_btn.grid(row=4, column=0, pady=5)
+    #??????????????????????
+    
+    ############# section des fonctions
+    
+
+    
+    def echelle( new_scaling: str):
+        new_scaling_float = int(new_scaling.replace("%", "")) / 100
+        customtkinter.set_widget_scaling(new_scaling_float)
+        with open(f"{placement}/scale.json", "w") as file:
+            json.dump({"scale":new_scaling_float}, file)
+        
+    def apparence( new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+        with open(f"{placement}/theme.json", "w") as file:
+            json.dump({"theme":new_appearance_mode}, file)
+            
+        #focntion pour sauvegarder les parametres           
+def saves():
+    #charger le fichier du theme        
+    if os.path.exists(f"{placement}/theme.json"):
+        with open(f"{placement}/theme.json", "r") as f:
+            theme = json.load(f)
+    #charger le fichier de l'echelle        
+    if os.path.exists(f"{placement}/scale.json"):
+        with open(f"{placement}/scale.json", "r") as f:
+            scale = json.load(f)
+    #organiser lesfichiers charge        
+    config = {
+            "scale": scale["scale"] or 1.0,
+            "theme": theme["theme"] or "System",
+        } 
+    #sauvegarder la configuration
+    with open(f"{placement}/config.json", "w") as confg:
+        json.dump(config, confg, indent=4) 
+        print("Sauvegarder avec succes") 
